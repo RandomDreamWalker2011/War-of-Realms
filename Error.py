0579faf320baa19e552f589531739a0d20f2f044
RandomDreamWalker 97062 @ENamnor <104535753+PSPFRDW97062@users.noreply.github.com>
2023-03-29 19:33:39 -0500

    Error Update: InputNotFoundError

--- Error.py
+++ Error.py
@@ -1,11 +1,21 @@
 """
     Custom Exceptions:
         - InitError
-            Runs when an Initialization Failed
+            Runs when an Initialization Fails
 
+        - NotFoundError
+            Runs when a function can't find an input
+            Replacement for a less suitable "TypeError"
 """
+
+
 class InitError(Exception):
     def __init__(self, message, class_name):
-        self.message = f"{class_name} initialization failed {message}."
+        self.message = f"{class_name} initialization failed: {message}."
         super().__init__(self.message)
 
+
+class InputNotFoundError(Exception):
+    def __init__(self, message):
+        self.message = f"{message}"
+        super().__init__(self.message)
